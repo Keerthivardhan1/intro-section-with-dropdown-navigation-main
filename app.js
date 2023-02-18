d390b80c9ebcb4f381daf0d307fbacae61e9df6c
keerthivardhan1 <keerthivardhantekulapelli@gmail.com>
2023-02-18 14:57:01 +0530

    initial commit1

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-let idl= document.getElementById("idl");
-let idra = document.getElementById("idra")
 
-idl.addEventListener("mouseover" , ()=>{
-    idra.style.border= "none";
+const four = document.querySelector(".four");
+const togglebtn = document.querySelector(".hamberger");
+const loginregister = document.getElementById("nav_right");
+const mobile_nav = document.querySelector(".nav_mobile")
+
+togglebtn.addEventListener("click" , ()=>{
+    console.log("clicked");
+    if(four.style.display == "none"){
+        four.style.display = "flex";            // no block its should be flex
+        loginregister.style.display = "block";
+        mobile_nav.style.height = "100vh";
+        togglebtn.style.backgroundImage = "url('icon-close-menu.svg')";
+        four.style.transform = "translateX(0%)"
+
+    }else{
+        four.style.display = "none";
+        loginregister.style.display = "none";
+        mobile_nav.style.height = "auto";
+        togglebtn.style.backgroundImage = "url('icon-menu.svg')"
+        four.style.transform = "translateX(100%)"
+    }
 })
-
-idl.addEventListener("mouseout" ,()=>{
-    idra.style.border = "2px solid hsl(0, 0%, 41%)"
-})
-
  
